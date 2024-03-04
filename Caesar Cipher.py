3afad83170ab22f65a6ecf4bca70056391564b19
Aero-Dans <152432346+Aero-Dans@users.noreply.github.com>
2024-03-04 13:10:07 -0500

    Create Caesar Cipher.py
    frfr

---
+++ Caesar Cipher.py
@@ -0,0 +1,18 @@
+# Daniel Zhang 349523548
+
+def encryptor(istring, svalue):
+    '''
+    encryptor functions uses the caeser cipher algorithm to encrypt a given string and the shift value
+
+    argument:
+    -- istring is the given string
+    -- svalue is the shift value
+    '''
+
+
+
+
+print("just do it")
+
+print(encryptor(input(), int(input())))
+
